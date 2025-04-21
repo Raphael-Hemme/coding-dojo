860961dff392f67015893aad460b5f2ab36e0ce6
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2025-04-21 15:54:31 +0200

    Add another kata solution - 202504211554

--- exercism/haskell/reverse-string.hs
+++ exercism/haskell/reverse-string.hs
@@ -1,9 +1,15 @@
 -- https://exercism.org/tracks/haskell/exercises/reverse-string
 -- easy - Reverse String
 
-module ReverseString (reverseStringV1) where
+module ReverseString (reverseStringV1, reverseStringV2) where
 
 -- V-1
 reverseStringV1 :: String -> String
 reverseStringV1 [] = []
 reverseStringV1 (x:xs) = reverseStringV1 xs ++ [x]
+
+-- V-2
+reverseStringV2 :: String -> String
+reverseStringV2 xs = case xs of
+    [] -> []
+    (x:xs) -> reverseStringV2 xs ++ [x]
