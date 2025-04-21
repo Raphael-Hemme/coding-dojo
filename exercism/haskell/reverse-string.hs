7d0e078a1a7139f0f74efb4bbd56a0ac36ac2d80
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2025-04-21 16:11:03 +0200

    Add another kata solution - 202504211611

--- exercism/haskell/reverse-string.hs
+++ exercism/haskell/reverse-string.hs
@@ -1,7 +1,7 @@
 -- https://exercism.org/tracks/haskell/exercises/reverse-string
 -- easy - Reverse String
 
-module ReverseString (reverseStringV1, reverseStringV2) where
+module ReverseString (reverseStringV1, reverseStringV2, reverseStringV3) where
 
 -- V-1
 reverseStringV1 :: String -> String
@@ -13,3 +13,8 @@ reverseStringV2 :: String -> String
 reverseStringV2 xs = case xs of
     [] -> []
     (x:xs) -> reverseStringV2 xs ++ [x]
+
+-- V-3
+reverseStringV3 :: String -> String
+reverseStringV3 [] = []
+reverseStringV3 (x:xs) = foldl (flip (:)) [x] xs
