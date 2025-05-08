a03461e66ecbaf0d8e155561cc4de6dbc3823f34
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2025-05-08 21:16:09 +0200

    Add another solution for JS kata 5b5604e26dc79e6832000101 - invertHash

--- codewars/js/5b5604e26dc79e6832000101.js
+++ codewars/js/5b5604e26dc79e6832000101.js
@@ -1,4 +1,8 @@
 // https://www.codewars.com/kata/5b5604e26dc79e6832000101/train/javascript
 // 7kyu - Inverting a hash
 
-export const invertHash = (hash) => Object.fromEntries(Object.entries(hash).map(([key, val]) => ([val, key])));
+// V-1
+export const invertHashV1 = (hash) => Object.fromEntries(Object.entries(hash).map(([key, val]) => ([val, key])));
+
+// V-2
+export const invertHash = (o) => Object.entries(o).reduce((acc, [k, v]) => ({ ...acc, [v]: k }), {});
